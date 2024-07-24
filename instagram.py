78e1199d895f78d7325436242b0fa4c5d0923286
Avanish Iyer <avanishiyer08@gmail.com>
2024-07-24 23:13:25 +0530

    changed the flow through messages instead of searching. Now works with groups as well

--- instagram.py
+++ instagram.py
@@ -53,27 +53,32 @@ class bot:
 		# notification pop-up
 		self.bot.find_element(By.XPATH,
 							'/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]').click()
+		time.sleep(2)
+
+		# message button
+		self.bot.find_element(By.XPATH,
+							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div[5]/div/div/span/div/a/div/div[2]/div/div/span/span').click()
 		time.sleep(5)
 
-		# search button
+		# send message button
 		self.bot.find_element(By.XPATH,
-							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div[2]/span/div/a/div/div[2]/div/div/span/span').click()
-		time.sleep(5)
+							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/section/div/div/div/div[1]/div/div[2]/div/div/div/div[4]/div').click()
+		time.sleep(2)
 
 		# enter the username
 		self.bot.find_element(By.XPATH,
-							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[1]/div/div/input').send_keys(user)
+							'/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/input').send_keys(user)
 		time.sleep(3)
 
 		# click on the account
 		self.bot.find_element(By.XPATH,
-							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/a[1]/div[1]/div/div/div[2]/div/div/div/span').click()
-		time.sleep(5)
+							'/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[3]/div/label/div/input').click()
+		time.sleep(1)
 
 		# messages button
 		self.bot.find_element(By.XPATH,
-							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[2]/div/div[1]/section/main/div/header/section[2]/div/div/div[2]/div/div[2]/div').click()
-		time.sleep(10)
+							'/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[4]/div').click()
+		time.sleep(5)
 
 		# click on text box
 		send = self.bot.find_element(By.XPATH,
