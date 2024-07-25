58e70070e32dd51009ac3bae3735d79a0dd62d92
Avanish Iyer <avanishiyer08@gmail.com>
2024-07-25 18:07:26 +0530

    added comments and spaces. also changed instagram.py to take input when ran instead of command line args

--- authenticator.py
+++ authenticator.py
@@ -1,35 +1,41 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
+import os
+import time
+import json
+import hmac
+import base64
+import struct
+import hashlib
 
-import hmac, base64, struct, hashlib, time, json, os
-
+# main magical function
 def get_hotp_token(secret, intervals_no):
-	"""This is where the magic happens."""
-	key = base64.b32decode(normalize(secret), True) # True is to fold lower into uppercase
+	key = base64.b32decode(normalize(secret), True)
 	msg = struct.pack(">Q", intervals_no)
 	h = bytearray(hmac.new(key, msg, hashlib.sha1).digest())
 	o = h[19] & 15
 	h = str((struct.unpack(">I", h[o:o+4])[0] & 0x7fffffff) % 1000000)
+
 	return prefix0(h)
 
 
+# The TOTP token is just a HOTP token seeded with every 30 seconds
 def get_totp_token(secret):
-	"""The TOTP token is just a HOTP token seeded with every 30 seconds."""
 	return get_hotp_token(secret, intervals_no=int(time.time())//30)
 
 
+# Normalizes secret by removing spaces and padding with = to a multiple of 8
 def normalize(key):
-	"""Normalizes secret by removing spaces and padding with = to a multiple of 8"""
 	k2 = key.strip().replace(' ','')
-	# k2 = k2.upper()	# skipped b/c b32decode has a foldcase argument
 	if len(k2)%8 != 0:
 		k2 += '='*(8-len(k2)%8)
+
 	return k2
 
-
+# Prefixes code with leading zeros if missing
 def prefix0(h):
-	"""Prefixes code with leading zeros if missing."""
 	if len(h) < 6:
 		h = '0'*(6-len(h)) + h
+
 	return h
 
 
--- instagram.py
+++ instagram.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import os
 import time
-import sys
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -15,7 +14,7 @@ class bot:
 	def __init__(self, username, password, twofa, receiver, message):
 		self.username = username
 		self.password = password
-		self.user = user
+		self.receiver = receiver
 		self.message = message
 		self.twofa = twofa
 		self.base_url = 'https://www.instagram.com/accounts/login/'
@@ -35,6 +34,7 @@ class bot:
 			expected_conditions.presence_of_element_located((By.NAME, 'password')))
 		enter_password.send_keys(self.password)
 
+		# press RETURN to go to next screen
 		enter_password.send_keys(Keys.RETURN)
 		time.sleep(5)
 
@@ -43,20 +43,21 @@ class bot:
 			expected_conditions.presence_of_element_located((By.NAME, 'verificationCode')))
 		enter_twofa.send_keys(self.twofa)
 
+		# press RETURN to go to next screen
 		enter_twofa.send_keys(Keys.ENTER)
 		time.sleep(10)
 
-		# save login pop-up
+		# save login pop-up --> press do not save
 		self.bot.find_element(By.XPATH,
 							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/div/div/div/div/div').click()
 		time.sleep(5)
 
-		# notification pop-up
+		# notification pop-up --> press not now
 		self.bot.find_element(By.XPATH,
 							'/html/body/div[3]/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[2]').click()
 		time.sleep(2)
 
-		# message button
+		# click messages button in the right collumn
 		self.bot.find_element(By.XPATH,
 							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[2]/div/div/div/div/div[2]/div[5]/div/div/span/div/a/div/div[2]/div/div/span/span').click()
 		time.sleep(5)
@@ -66,43 +67,55 @@ class bot:
 							'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/section/div/div/div/div[1]/div/div[2]/div/div/div/div[4]/div').click()
 		time.sleep(2)
 
-		# enter the username
+		# enter the username in input field
 		self.bot.find_element(By.XPATH,
-							'/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/input').send_keys(user)
+							'/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[2]/div/div[2]/input').send_keys(self.receiver)
 		time.sleep(3)
 
-		# click on the account
+		# click the tick-box to select the account
 		self.bot.find_element(By.XPATH,
 							'/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[3]/div/label/div/input').click()
 		time.sleep(1)
 
-		# messages button
+		# click 'chat' button
 		self.bot.find_element(By.XPATH,
 							'/html/body/div[7]/div[1]/div/div[2]/div/div/div/div/div/div/div[1]/div/div[4]/div').click()
 		time.sleep(5)
 
-		# click on text box
+		# click on messages text box
 		send = self.bot.find_element(By.XPATH,
 									'/html/body/div[2]/div/div/div[2]/div/div/div[1]/div[1]/div[1]/section/main/section/div/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div[2]/div/div/div[2]/div/div/div[2]/div/div[1]')
 		send.click()
 		time.sleep(1)
 
-		# type message
+		# type the message
+		# used for loop to solve issue where it won't send the whole message, just the first letter
 		for i in self.message:
 			send.send_keys(i)
-		time.sleep(5)
+		time.sleep(1)
 
-		# send message
+		# pressing RETURN to sent the message
 		send.send_keys(Keys.RETURN)
-		time.sleep(5)
+		time.sleep(2)
+
+		# closing browser
+		self.bot.close()
 
 
+# calling the main function through an initialising function
 def init(username, password, two_FA_Code, reciever, message):
 	bot(username, password, two_FA_Code, reciever, message)
 
 	print("DONE")
 
 auth_code = authenticator.returnTwoFA('Instagram')
-user = sys.argv[1]
-message_ = sys.argv[2]
-init('zenderwastaken', '02jISG#SNuP5krJ1', auth_code, user, message_)
+account_username = 'zenderwastaken'
+account_password = '02jISG#SNuP5krJ1'
+receiver_username = ''
+message_ = ''
+
+print('Using account: ' + account_username)
+receiver_username = input('Enter reciever username: ')
+message_ = input('Message: ')
+
+init(account_username, account_password, auth_code, receiver_username, message_)
